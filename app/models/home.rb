174792dead71365e2cec4d811ed86d5dbdc930fe
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-28 11:30:20 -0600

    Data validations for home model

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,4 +1,14 @@
 class Home < ApplicationRecord
+  validates_presence_of :spark_id
+  validates_presence_of :mls_id
+  validates_presence_of :list_price
+  validates_presence_of :address
+  validates_presence_of :total_baths
+  validates_presence_of :total_beds
+  validates_presence_of :garage_spaces
+  validates_presence_of :lot_size
+  validates_presence_of :sqft
+
   def self.from_json(json)
     home = new
     binding.pry
