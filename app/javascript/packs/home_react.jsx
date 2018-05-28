c0952c2ad5db58abcb84a6020bde20faad176d51
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-28 16:50:03 -0600

    Add fetch shim using whatwg-fetch

--- app/javascript/packs/home_react.jsx
+++ app/javascript/packs/home_react.jsx
@@ -1,15 +1,18 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import Home from '../bundles/Lifestyle/components/Home'
+import 'whatwg-fetch'
 
 document.addEventListener('DOMContentLoaded', () => {
+  fetch('/api/v1/homes/featured').then(response => {
+    return response.json();
+  }).then(json => {
+    console.log(json);
+  });
+
   ReactDOM.render(
     <Home list_price={10} first_line_address="Address" />,
     document.body.appendChild(document.createElement('div')),
-  )
+  );
 })
