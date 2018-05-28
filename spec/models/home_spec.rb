2d1f61a4a0103082f09681dc8023e2067be5e43c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-28 11:17:43 -0600

    Spec for home model, only requiring basic fields for now

---
+++ spec/models/home_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Home, type: :model do
+  it 'should be able to load a home from JSON' do
+    home_json = File.read('fixtures/home.json')
+
+    home = Home.from_json(home_json)
+
+    expect(home).to be_a Home
+
+    standard_fields = home_json[:StandardFields]
+
+    expect(home.list_price).to eq(standard_fields[:ListPrice])
+    expect(home.address).to eq(standard_fields[:UnparsedAddress])
+    expect(home.total_baths).to eq(standard_fields[:BathsTotal])
+    expect(home.total_beds).to eq(standard_fields[:BedsTotal])
+    expect(home.garage_spaces).to eq(standard_fields[:GarageSpaces])
+    expect(home.lot_size).to eq(standard_fields[:LotSizeAcres])
+    expect(home.sqft).to eq(standard_fields[:BuildingAreaTotal])
+    expect(home.agent).to be_an Agent
+  end
+end
