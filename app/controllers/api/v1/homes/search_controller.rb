54799baab2c89ab227c9509a1bdab679f6110fbb
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-28 12:46:12 -0600

    SearchController#index for home

--- app/controllers/api/v1/homes/search_controller.rb
+++ app/controllers/api/v1/homes/search_controller.rb
@@ -1,4 +1,22 @@
 class Api::V1::Homes::SearchController < ApplicationController
   def index
+    results = SparkService.search(search_params)
+    binding.pry
+  end
+
+  private
+
+  def search_params
+    params.permit(
+      :zip,
+      :minPrice,
+      :maxPrice,
+      :minBaths,
+      :maxBaths,
+      :minSqft,
+      :maxSqft,
+      :minBeds,
+      :maxBeds
+    )
   end
 end
