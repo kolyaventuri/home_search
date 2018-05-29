8b38af2be3b15306e7a1e33bbf8c3f96fad0b236
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-29 11:08:24 -0600

    Migration to add small hero shot to home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_035641) do
+ActiveRecord::Schema.define(version: 2018_05_29_170806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_035641) do
     t.datetime "updated_at", null: false
     t.string "first_line_address"
     t.string "hero_shot"
+    t.string "hero_shot_small"
   end
 
 end
