93bb40964fa6a0469100bff234b9b0eb1cd61efb
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-29 00:49:36 -0600

    Set up BedsBaths component for modularity

--- app/javascript/bundles/Lifestyle/components/listing/BedsBaths.jsx
+++ app/javascript/bundles/Lifestyle/components/listing/BedsBaths.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Beds from './items/Beds'
+import Baths from './items/Baths'
+import Garage from './items/Garage'
 
-const BedsBaths = ({ address, price, sqft }) => {
+const BedsBaths = ({ beds, baths, garage }) => {
     return (
         <div className='column is-narrow is-three-quarters columns'>
             <div className='column'>
                 <div className='info'>
+                    <Beds>{ beds }</Beds>
+                    <Baths>{ baths }</Baths>
+                    <Garage>{ garage }</Garage>
                 </div>
             </div>
         </div>
