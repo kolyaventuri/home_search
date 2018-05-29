2dee0b0eeb60f928891a8725d9aac911efcfb40b
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-29 01:11:44 -0600

    Add animations during results loading

--- app/javascript/bundles/Lifestyle/components/listing/Info.jsx
+++ app/javascript/bundles/Lifestyle/components/listing/Info.jsx
@@ -4,7 +4,7 @@ import BedsBaths from './BedsBaths'
 
 const Info = ({ address, price, sqft, beds, baths, garage }) => {
     return (
-        <div className='column is-narrow is-three-quarters columns'>
+        <div className='column is-narrow is-three-quarters columns info'>
             <BasicInfo { ...{ address, price, sqft } } />
             <BedsBaths { ...{ beds, baths, garage } } />
         </div>
