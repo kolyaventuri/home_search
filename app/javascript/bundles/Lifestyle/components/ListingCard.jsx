cccd96d390cf1c4d0dfb1885fa516758b61de4c1
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-28 21:52:11 -0600

    Render listing hero shot on component

--- app/javascript/bundles/Lifestyle/components/ListingCard.jsx
+++ app/javascript/bundles/Lifestyle/components/ListingCard.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const Listing = ({ hero, address, price }) => (
-    <div className={'column is-narrow home ' + (address ? '': 'loading')}>
-        <div data-attribute="hero"><img src={hero} /></div>
-        <p data-attribute="address">{ address }</p>
-        <p data-attribute="price">{ price }</p>
-    </div>
-);
+const Listing = ({ hero, address, price }) => {
+    const listingStyle = {
+        backgroundImage: 'url(' + hero + ')',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+    };
+
+    return (
+        <div className={'column is-narrow home ' + (address ? '': 'loading')} style={listingStyle}>
+            <div data-attribute="hero"><img src={hero} /></div>
+            <p data-attribute="address">{ address }</p>
+            <p data-attribute="price">{ price }</p>
+        </div>
+   );
+};
 
 export default Listing;
