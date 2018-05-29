69bdd8eb9243f3231ec63f741f9d9b2dadd2e8f7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-29 00:33:05 -0600

    Better passing of props down to sub componenet

--- app/javascript/bundles/Lifestyle/components/ListingSearchResult.jsx
+++ app/javascript/bundles/Lifestyle/components/ListingSearchResult.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ListingPhoto from './listing/Photo'
 import BasicInfo from './listing/BasicInfo'
 
-const Listing = ({ hero, address, price }) => {
+const Listing = ({ hero, address, price, baths, beds, garage, sqft, lot_size }) => {
 
     return (
         <div className={'column is-narrow home columns ' + (address ? '': 'loading')}>
             <ListingPhoto hero={ hero } />
-            <BasicInfo address={ address } price={ price } />
+            <BasicInfo { ...{ address, price, sqft } } />
+
         </div>
    );
 };
