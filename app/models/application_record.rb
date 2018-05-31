da32a530a0d7b2ef8b38cf7ba55880c60967643e
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-31 15:08:39 -0600

    Add sample method to all models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,2 +1,14 @@
 class ApplicationRecord
+  def self.sample(num=1)
+    num = 1 if num < 1
+    self.collection.aggregate(
+      [
+        {
+          '$sample': {
+            size: num
+          }
+        }
+      ]
+    )
+  end
 end
