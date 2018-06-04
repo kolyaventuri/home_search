93cf3c90126e375f5a3f944275f75f45a0306479
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-04 00:27:17 -0600

    Query builder for mongoDB searches

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,5 +1,42 @@
 class SearchService
   def self.search(params)
+    query = new.map_params(params)
+    binding.pry
+  end
 
+  def map_params(params)
+    query_hash = {}
+    prefix = 'StandardFields.'
+
+
+    params.each_pair do |param, value|
+      next if map[param].nil?
+
+      name = map[param].first
+      comparison = map[param][1]
+      modifier = map[param][2]
+
+      name = name.send(modifier) unless modifier.nil?
+
+      query_hash["#{prefix}#{name}"] = { comparison.to_sym => value }
+    end
+
+    query_hash
+  end
+
+  private
+
+  def map
+    {
+      "zip" => ['PostalCode', "$eq", :to_s],
+      "minPrice" => ['ListPrice', "$gte"],
+      "maxPrice" => ['ListPrice', "$lte"],
+      "minBaths" => ['BathsTotal', "$gte"],
+      "maxBaths" => ['BathsTotal', "$lte"],
+      "minSqft" => ['BuildingAreaTotal', "$gte"],
+      "maxSqft" => ['BuildingAreaTotal', "$lte"],
+      "minBeds" => ['BedsTotal', "$gte"],
+      "maxBeds" => ['BedsTotal', "$lte"]
+    }
   end
 end
