56d50afdac0970b3ce9bfaf0ae9a3c58e8e15be2
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-06 10:21:15 -0600

    Cotnroller accepts parameters for yelp search

--- app/controllers/lifestyle_controller.rb
+++ app/controllers/lifestyle_controller.rb
@@ -1,13 +1,43 @@
 class LifestyleController < ApplicationController
   def index
     session[:search_params] = search_params.to_json
+    @nums = gen_nums(5)
+  end
+
+  def create
+    q = JSON.parse(session[:search_params])['q']
   end
 
   private
 
+  def gen_nums(max)
+    nums = Array.new(max)
+
+    nums = nums.map.with_index do |n, index|
+      [index + 1, index + 1]
+    end
+
+    nums
+  end
+
+
+  private
+
+  def ranking_params
+    params.require(:importance).permit(
+      :food,
+      :entertainment,
+      :active
+    ).transform_values(&:to_i)
+  end
+
   def search_params
     params.permit(
-      :q,
+      :page,
+      :location,
+      :radius,
+      :zip,
+      :address,
       :minPrice,
       :maxPrice,
       :minBaths,
