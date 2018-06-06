2382ebe1cd5a08a1d82993d3a939bf5e9a3dd065
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-05 22:56:35 -0600

    [HOTFIX] Only look up favorites if user is logged in

--- app/controllers/api/v1/homes/search_controller.rb
+++ app/controllers/api/v1/homes/search_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::Homes::SearchController < ApplicationController
   def index
     search_results = SearchService.search(search_params)
-    home_ids = current_user.home_ids.map(&:to_s)
+    if current_user
+      home_ids = current_user.home_ids.map(&:to_s)
 
-    search_results[:results].map! do |home|
-      home[:favorite] = home_ids.include?(home[:id].to_s) if current_user
-      home.delete(:id)
-      home
+      search_results[:results].map! do |home|
+        home[:favorite] = home_ids.include?(home[:id].to_s) if current_user
+        home.delete(:id)
+        home
+      end
     end
 
     render json: search_results
