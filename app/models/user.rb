629d5a290c9444f2f8037c8079f8b2cd5f26c6f6
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-05 20:58:03 -0600

    Can create user from omniauth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   field :uid
   validates_uniqueness_of :uid
 
+  def self.from_omniauth(auth_info)
+    find_or_create_by(uid: auth_info['uid']).update(auth_info)
+    find_by(uid: auth_info['uid'])
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,10 @@ describe User, type: :moel do
 
     expect(user2).to_not be_valid
   end
+
+  it 'should be able to create a user from an OmniAuth hash' do
+    user = User.from_omniauth(mock_auth_hash.to_hash)
+
+    expect(user).to be_valid
+  end
 end
