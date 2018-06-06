62937abcdc7b25d9f5acd75ff9f935d4a345179d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-05 21:42:53 -0600

    Delete ID from search results as we only use it in the controller

--- app/controllers/api/v1/homes/search_controller.rb
+++ app/controllers/api/v1/homes/search_controller.rb
@@ -2,11 +2,10 @@ class Api::V1::Homes::SearchController < ApplicationController
   def index
     search_results = SearchService.search(search_params)
 
-    if current_user
-      search_results[:results].map! do |home|
-        home[:favorite] = !current_user.homes.where(Id: home[:Id]).empty?
-        home
-      end
+    search_results[:results].map! do |home|
+      home[:favorite] = !current_user.homes.where(Id: home[:Id]).empty? if current_user
+      home.delete(:Id)
+      home
     end
 
     render json: search_results
